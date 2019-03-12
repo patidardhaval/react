12f72412592fae8df3ca3ae5856608f8e44a330a
ubuntu <ubantu@yahoo.in>
2019-03-12 11:02:14 +0530

    init

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
-import Home from "./Home";
-import Stuff from "./Stuff";
-import Contact from "./Contact";
+import Home from "./View/Home";
+import Stuff from "./View/Stuff";
+import Contact from "./View/Contact";
 
 class Main extends Component {
   render() {
@@ -11,12 +11,12 @@ class Main extends Component {
         <div>
           <h1>Simple SPA</h1>
           <ul className="header">
-            <li><NavLink activeClassName="active" exact to="/">Home</NavLink></li>
+            <li><NavLink exact activeClassName="active"  to="/">Home</NavLink></li>
             <li><NavLink activeClassName="active" to="/stuff">Stuff</NavLink></li>
             <li><NavLink activeClassName="active" to="/contact">Contact</NavLink></li>
           </ul>
           <div className="content">
-            <Route  path="/" component={Home}/>
+            <Route exact path="/" component={Home}/>
             <Route path="/stuff" component={Stuff}/>
             <Route path="/contact" component={Contact}/>
           </div>
