191daffeae8f976de5c477de5d989766abe18d61
ubuntu <ubantu@yahoo.in>
2019-03-13 18:33:55 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://patidardhaval.github.io/react/precache-manifest.a4860cf31d317b87c3bc6bb81bab443d.js"
+  "/react/precache-manifest.435d09cb1d3a8f7cd896ddd34b3fd7ed.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://patidardhaval.github.io/react/index.html", {
+workbox.routing.registerNavigationRoute("/react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
