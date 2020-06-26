c0e26e73190564464887d0b5d82c4e5ccb547c61
Dhaval Patel <dhavalpatel@Dhavs-MacBook-Pro.local>
2020-06-26 18:02:36 +0530

    Updates

---
+++ precache-manifest.b128a463d7010f983e1775ff106bee83.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0ef0fafa6ee4128ee8b4d5c63beb29",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "240ee951fdda26028297",
+    "url": "/react/static/css/2.d805e148.chunk.css"
+  },
+  {
+    "revision": "0663c4f0548a75eec60d",
+    "url": "/react/static/css/main.fb03f4d4.chunk.css"
+  },
+  {
+    "revision": "240ee951fdda26028297",
+    "url": "/react/static/js/2.60a45082.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/2.60a45082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a656ee38a7d35f0648a5",
+    "url": "/react/static/js/3.9f6df007.chunk.js"
+  },
+  {
+    "revision": "a3859fb5c3c9c58df229",
+    "url": "/react/static/js/4.648a9132.chunk.js"
+  },
+  {
+    "revision": "bdc321f265692014ee1f",
+    "url": "/react/static/js/5.9e84a5e1.chunk.js"
+  },
+  {
+    "revision": "0663c4f0548a75eec60d",
+    "url": "/react/static/js/main.25ba1e21.chunk.js"
+  },
+  {
+    "revision": "2f09fd25b91b732ec5e5",
+    "url": "/react/static/js/runtime-main.3c65ba95.js"
+  }
+]);
